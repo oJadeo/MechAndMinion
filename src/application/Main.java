2b5b665d07fc0156399ba49b018664b9cd3edada
first122 <napassakorn2@hotmail.co.th>
2020-04-09 13:09:53 +0700

    0904

--- src/application/Main.java
+++ src/application/Main.java
@@ -10,6 +10,7 @@ import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
+import javafx.scene.effect.ColorAdjust;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
@@ -39,108 +40,118 @@ public class Main extends Application {
 	static Canvas[] draftedCardCanvas = new Canvas[6];
 	Scene firstScene;
 	Stage window;
+	ImageView tu;
+	int pageTuorial;
 	
 	@Override
 	public void start(Stage primaryStage) {
 		
 		VBox menu = new VBox();
-		menu.setSpacing(5.0);
-		BackgroundImage myImage= new BackgroundImage(new Image("WallpaperFirstScene.png",1380,680,false,true),
+		menu.setSpacing(20);
+		BackgroundImage myImage= new BackgroundImage(new Image("Wallpaper.jpg",1380,680,false,true),
 		        BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,
 		          BackgroundSize.DEFAULT);
 		menu.setBackground(new Background(myImage));
 		menu.setAlignment(Pos.CENTER);
+		menu.getChildren().add(new ImageView(new Image("Logo.png")));
         
-		Button start = new Button();
-		start.setBorder(new Border(new BorderStroke(Color.YELLOW, BorderStrokeStyle.SOLID, 
-				CornerRadii.EMPTY, BorderWidths.DEFAULT)));
-		Image imagePressStart = new Image("PressStart.png");		
-		ImageView imagePressStartView = new ImageView(imagePressStart);
-		start.setGraphic(imagePressStartView);	
+		VBox start = new VBox();
+		start.setAlignment(Pos.CENTER);
+		ImageView imageViewStart = new ImageView(new Image("Start.png"));
+		start.getChildren().add(imageViewStart);
 		menu.getChildren().add(start);
 		
-		Button tutorial = new Button();
-		tutorial.setBorder(new Border(new BorderStroke(Color.YELLOW, BorderStrokeStyle.SOLID, 
-				CornerRadii.EMPTY, BorderWidths.DEFAULT)));
-		Image imageTutorial = new Image("Tutorial.png");		
-		ImageView imageTutorialView = new ImageView(imageTutorial);
-		tutorial.setGraphic(imageTutorialView);	
+		VBox tutorial = new VBox();
+		tutorial.setAlignment(Pos.CENTER);
+		ImageView imageViewTutorial = new ImageView(new Image("Tutorial.png"));
+		tutorial.getChildren().add(imageViewTutorial);
 		menu.getChildren().add(tutorial);
 		
-		Button exit = new Button();
-		exit.setBorder(new Border(new BorderStroke(Color.YELLOW, BorderStrokeStyle.SOLID, 
-				CornerRadii.EMPTY, BorderWidths.DEFAULT)));
-		Image imageExit = new Image("Exit.png");		
-		ImageView imageExitView = new ImageView(imageExit);
-		exit.setGraphic(imageExitView);	
+		VBox exit = new VBox();
+		exit.setAlignment(Pos.CENTER);
+		ImageView imageViewExit = new ImageView(new Image("Exit.png"));
+		exit.getChildren().add(imageViewExit);
 		menu.getChildren().add(exit);
 		
 		
-		start.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent event) {
+		start.setOnMouseClicked(new EventHandler<MouseEvent>() {
+
+			public void handle(MouseEvent arg0) {
 				GameController.initializeGame();
 				primaryStage.setScene(update());
 				primaryStage.show();
+				
 			}
 		});
 		start.setOnMouseEntered(new EventHandler<MouseEvent>() {
+
 			@Override
-			public void handle(MouseEvent event) {
-				start.setPrefWidth(400);
-				start.setPrefHeight(66);
+			public void handle(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				ColorAdjust colorAdjust = new ColorAdjust(0,0,0.3,0);
+			    imageViewStart.setEffect(colorAdjust);
 			}
 		});
 		start.setOnMouseExited(new EventHandler<MouseEvent>() {
+
 			@Override
-			public void handle(MouseEvent event) {
-				start.setPrefWidth(300);
-				start.setPrefHeight(50);
+			public void handle(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				ColorAdjust colorAdjust = new ColorAdjust(0,0,0,0);
+			    imageViewStart.setEffect(colorAdjust);
 			}
 		});
-		tutorial.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent event) {
+		tutorial.setOnMouseClicked(new EventHandler<MouseEvent>() {
+
+			public void handle(MouseEvent arg0) {
 				primaryStage.setScene(tutorialGame());
 				primaryStage.show();
+				
 			}
 		});
 		tutorial.setOnMouseEntered(new EventHandler<MouseEvent>() {
+
 			@Override
-			public void handle(MouseEvent event) {
-				tutorial.setPrefWidth(400);
-				tutorial.setPrefHeight(66);
+			public void handle(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				ColorAdjust colorAdjust = new ColorAdjust(0,0,0.3,0);
+			    imageViewTutorial.setEffect(colorAdjust);
 			}
 		});
 		tutorial.setOnMouseExited(new EventHandler<MouseEvent>() {
+
 			@Override
-			public void handle(MouseEvent event) {
-				tutorial.setPrefWidth(300);
-				tutorial.setPrefHeight(50);
+			public void handle(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				ColorAdjust colorAdjust = new ColorAdjust(0,0,0,0);
+			    imageViewTutorial.setEffect(colorAdjust);
 			}
 		});
-		
-		exit.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent event) {
+		exit.setOnMouseClicked(new EventHandler<MouseEvent>() {
+			
+			public void handle(MouseEvent arg0) {
 				primaryStage.close();
+				
 			}
 		});
 		exit.setOnMouseEntered(new EventHandler<MouseEvent>() {
+
 			@Override
-			public void handle(MouseEvent event) {
-				exit.setPrefWidth(400);
-				exit.setPrefHeight(66);
+			public void handle(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				ColorAdjust colorAdjust = new ColorAdjust(0,0,0.3,0);
+			    imageViewExit.setEffect(colorAdjust);
 			}
 		});
 		exit.setOnMouseExited(new EventHandler<MouseEvent>() {
+
 			@Override
-			public void handle(MouseEvent event) {
-				exit.setPrefWidth(300);
-				exit.setPrefHeight(50);
+			public void handle(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				ColorAdjust colorAdjust = new ColorAdjust(0,0,0,0);
+			    imageViewExit.setEffect(colorAdjust);
 			}
 		});
-		
 		
 		firstScene = new Scene(menu,1380,680);
 		window = primaryStage;
@@ -151,28 +162,144 @@ public class Main extends Application {
 	}
 	
 	public Scene tutorialGame() {
-		GridPane root = new GridPane();
-		BackgroundImage image= new BackgroundImage(new Image("WallpaperFirstScene.png",1380,680,false,true),
+		int minPage = 1;
+		int maxPage = 7;
+		VBox root = new VBox();
+		BackgroundImage image= new BackgroundImage(new Image("Wallpaper.jpg",1380,680,false,true),
 		        BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,
 		          BackgroundSize.DEFAULT);
 		root.setBackground(new Background(image));
+		root.setSpacing(20);
 		
 		
 		
-		Button back = new Button();
-		back.setBorder(new Border(new BorderStroke(Color.YELLOW, BorderStrokeStyle.SOLID, 
-				CornerRadii.EMPTY, BorderWidths.DEFAULT)));
-		Image imageBack = new Image("Back.png");		
-		ImageView imageStartView = new ImageView(imageBack);
-		back.setGraphic(imageStartView);	
-		back.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent event) {
+		HBox upRoot = new HBox();
+		VBox back = new VBox();
+		back.setAlignment(Pos.TOP_LEFT);
+		ImageView imageViewBack = new ImageView(new Image("Back.png"));
+		back.getChildren().add(imageViewBack);
+		upRoot.getChildren().add(back);
+		back.setOnMouseClicked(new EventHandler<MouseEvent>() {
+			public void handle(MouseEvent arg0) {
 				window.setScene(firstScene);
+				window.show();
+			}
+		});
+		back.setOnMouseEntered(new EventHandler<MouseEvent>() {
+
+			@Override
+			public void handle(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				ColorAdjust colorAdjust = new ColorAdjust(0,0,0.3,0);
+			    imageViewBack.setEffect(colorAdjust);
+			}
+		});
+		back.setOnMouseExited(new EventHandler<MouseEvent>() {
+
+			@Override
+			public void handle(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				ColorAdjust colorAdjust = new ColorAdjust(0,0,0,0);
+			    imageViewBack.setEffect(colorAdjust);
 			}
 		});
 		
-		root.add(back,0,0,2,1);
+		
+		HBox midRoot = new HBox();
+		midRoot.setAlignment(Pos.TOP_CENTER);
+		tu = new ImageView(new Image("page1.png"));
+		midRoot.getChildren().add(tu);
+		
+		
+		
+		HBox lowRoot = new HBox();
+		lowRoot.setSpacing(20);
+		lowRoot.setAlignment(Pos.CENTER);
+		VBox arrowLeft = new VBox();
+		ImageView imageViewArrowLeft = new ImageView();
+		arrowLeft.getChildren().add(imageViewArrowLeft);
+		lowRoot.getChildren().add(arrowLeft);
+		VBox arrowRight = new VBox();
+		ImageView  imageViewArrowRight = new ImageView(new Image("ArrowRight.png"));
+		arrowRight.getChildren().add(imageViewArrowRight);
+		lowRoot.getChildren().add(arrowRight);
+		arrowRight.setOnMouseClicked(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent arg0) {
+				if(pageTuorial < maxPage) {
+					if(pageTuorial == maxPage-1) {
+						imageViewArrowRight.setImage(null);
+					}
+					if(pageTuorial == minPage) {
+						imageViewArrowLeft.setImage(new Image("ArrowLeft.png"));
+					}
+					pageTuorial += 1;
+					tu = new ImageView(new Image("Page" + pageTuorial + ".png"));
+					midRoot.getChildren().remove(0);
+					midRoot.getChildren().add(tu);
+				}
+			}
+		});
+		arrowRight.setOnMouseEntered(new EventHandler<MouseEvent>() {
+
+			@Override
+			public void handle(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				ColorAdjust colorAdjust = new ColorAdjust(0,0,0.3,0);
+			    imageViewArrowRight.setEffect(colorAdjust);
+			}
+		});
+		arrowRight.setOnMouseExited(new EventHandler<MouseEvent>() {
+
+			@Override
+			public void handle(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				ColorAdjust colorAdjust = new ColorAdjust(0,0,0,0);
+				imageViewArrowRight.setEffect(colorAdjust);
+			}
+		});
+		arrowLeft.setOnMouseClicked(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent arg0) {
+				if(pageTuorial > minPage) {
+					if(pageTuorial == minPage+1) {
+						imageViewArrowLeft.setImage(null);
+					}
+					if(pageTuorial == maxPage) {
+						imageViewArrowRight.setImage(new Image("ArrowRight.png"));
+					}
+					pageTuorial -= 1;
+					tu = new ImageView(new Image("Page" + pageTuorial + ".png"));
+					midRoot.getChildren().remove(0);
+					midRoot.getChildren().add(tu);
+				}
+			}
+		});
+		arrowLeft.setOnMouseEntered(new EventHandler<MouseEvent>() {
+
+			@Override
+			public void handle(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				ColorAdjust colorAdjust = new ColorAdjust(0,0,0.3,0);
+				imageViewArrowLeft.setEffect(colorAdjust);
+			}
+		});
+		arrowLeft.setOnMouseExited(new EventHandler<MouseEvent>() {
+
+			@Override
+			public void handle(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				ColorAdjust colorAdjust = new ColorAdjust(0,0,0,0);
+				imageViewArrowLeft.setEffect(colorAdjust);
+			}
+		});
+		
+		
+
+		
+		root.getChildren().add(upRoot);
+		root.getChildren().add(midRoot);
+		root.getChildren().add(lowRoot);
 		Scene scene = new Scene(root,1380,680);
 		return scene;
 	}
@@ -180,7 +307,8 @@ public class Main extends Application {
 	
 	public Scene update() {
 		VBox root = new VBox();
-
+		
+		
 		HBox boardRoot = new HBox();
 		Canvas boardCanvas = new Canvas(480, 480);
 		GraphicsContext boardGC = boardCanvas.getGraphicsContext2D();
